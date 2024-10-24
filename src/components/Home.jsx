8d572244699c3fdd814d3dbca3e0607865c28597
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-10-23 22:00:59 -0500

    feat :lipstick: se creo el diseño de la nota vacía
    en proceso de importar las imagenes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,32 @@
-
 import styles from '../styles/Home.module.css';
+import add from '../storage/img/add.png';
+import cuate from '../storage/img/cuate.png';
 
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className={styles.ejemplo}>fhsdfs</h1>
-    </div>
-  )
-}
 
-export default Home
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Notes</div>
+      <div className={styles.noteMessage}>Create your first note!</div>
+      <img className={styles.image} src= {cuate} alt="Placeholder" />
+      <div className={styles.button1} />
+      <div className={styles.icon1}>
+        {/* Asegúrate de tener contenido aquí, como un ícono */}
+        <img src="" alt="Icon 1" />
+      </div>
+      <div className={styles.button2} />
+      <div className={styles.icon2}>
+        {/* Asegúrate de tener contenido aquí, como un ícono */}
+        <img src="../storage/img/add.png" alt="Icon 2" />
+      </div>
+      <div className={styles.roundedButton} />
+      <div className={styles.roundedIcon}>
+        {/* Asegúrate de tener contenido aquí, como un ícono */}
+        <img src={add} alt="Rounded Icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
